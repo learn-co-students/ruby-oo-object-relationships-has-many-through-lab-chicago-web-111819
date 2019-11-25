a860a61d1a17164dbf39531e121ba174a7a83973
dut-a <d.dut@live.com>
2019-11-25 13:34:29 -0600

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,31 @@
+require_relative 'appointment'
+
+class Doctor
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(patient, date)
+       Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select { |appt| appt.doctor == self }
+    end
+
+    def patients
+        appointments.collect { |appt| appt.patient }
+    end
+
+
+end
